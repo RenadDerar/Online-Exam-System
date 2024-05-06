e2bd1fd5d84f95c2231bf86cb0c97ce702c43b59
Sondoskhalid2005 <162904939+Sondoskhalid2005@users.noreply.github.com>
2024-05-06 06:18:03 +0300

    Update #2 editedModifyFunction.cpp

--- editedModifyFunction.cpp
+++ editedModifyFunction.cpp
@@ -1,23 +1,57 @@
 
-void displayQuestion(int ARRAYSIZE1); // function to display the questions it takes the number of questions enterd by the teacher
+void displayQuestion(question questiontype[]); // function to display the questions it takes the number of questions enterd by the teacher
 
-void modifyexam(question questiontype[], int ARRAYSIZE1);
+void modifyexam(question questiontype[]);
 
-void modifyexam(question questiontype[], int ARRAYSIZE1)
+int main() {
+
+
+    for (int i = 0; i < 3; i++)
+    {
+        getline(cin, chemistryQuestionList[i].questionText);
+
+
+        for (int j = 0; j < 4; j++)
+        {
+            getline(cin, chemistryQuestionList[i].questionAnswer[j].choiceText);
+
+        }
+    }
+    modifyexam(chemistryQuestionList);
+    /*/for (int i = 0; i < 10; i++)
+    {
+        cout << chemistryQuestionList[i].questionText;
+
+
+        for (int j = 0; j < 4; j++)
+        {
+            cout << chemistryQuestionList[i].questionAnswer[j].choiceText;
+}
+
+        }/*/
+    displayQuestion(chemistryQuestionList);
+    return 0;
+
+}
+
+void modifyexam(question questiontype[])
 {
-    char answer, choiceAnswer;
+    int answer;
+    char choiceAnswer;
     int deletedindex, editedquestionIndex, editQuestionChoiceIndex, editChoiceIndex;
-    
+
     bool loopcondition = true;
 
     while (loopcondition == true) {
 
-        displayQuestion(ARRAYSIZE1); //to display after each change in question list
-        cout << "press: d for deleting a Question " << endl;
-        cout << "press: e for editing a Question " << endl;
-        cout << "press: f for editing a choice " << endl;
-        cout << "press: n to exite" << endl;
+        displayQuestion(questiontype);
+        //to display after each change in question list
+        cout << "press: 1 for deleting a Question " << endl;
+        cout << "press: 2 for editing a Question " << endl;
+        cout << "press: 3 for editing a choice " << endl;
+        cout << "press: 4 to exite" << endl;
         cin >> answer;
+        cin.ignore();
 
 
 
@@ -26,55 +60,78 @@ void modifyexam(question questiontype[], int ARRAYSIZE1)
 
             //     case he want to delete a Question
 
-        case 'd':
-        case 'D':
+
+        case 1:
         {
             cout << "enter number of Question you want to delete" << endl;
             cin >> deletedindex;
-            for (int i = deletedindex; i < ARRAYSIZE; i++) {
+            cin.ignore();
+
+            for (int i = deletedindex; i < 10; i++) {
                 questiontype[i - 1] = questiontype[i];
             }
-            ARRAYSIZE1--; // to decreas number of questions after deleting  
+
+            // to stop repeating the last element
+            for (int i = 0; i < 9; i++) {
+                for (int j = i + 1; j < 10; j++) {
+                    
+                    if (questiontype[i].questionText == questiontype[j].questionText)
+                    {
+                        questiontype[j].questionText = "";
+                    }
+
+
+                }
+            }
         }
         break;
-        
+
         //      case he want to edit a question
-        case 'e':
-        case 'E':
+
+        case 2:
         {
             cout << "enter number of Question you want to edit" << endl;
             cin >> editedquestionIndex;
+            cin.ignore();
             //enter the question update
             cout << "enter the edited Question" << endl;
-             getline(cin,questiontype[editedquestionIndex - 1].questionText);
+            getline(cin, questiontype[editedquestionIndex - 1].questionText);
             cin.ignore();
+            
+            
+
         }
         break;
-        
+
         //     case he want to edit a choice
-        case 'f':
-        case 'F':
+
+        case 3:
         {
             cout << "enter number of Question you want to edit its choice" << endl;
             cin >> editQuestionChoiceIndex;
+            cin.ignore();
+
 
             cout << "enter number of choice you want to edit " << endl;
             cin >> editChoiceIndex;
+            cin.ignore();
+
 
             //  enter the choice update
             cout << "enter the edited choice" << endl;
-             getline(cin,questiontype[editQuestionChoiceIndex - 1].questionAnswer[editChoiceIndex - 1].choiceText);
+            getline(cin, questiontype[editQuestionChoiceIndex - 1].questionAnswer[editChoiceIndex - 1].choiceText);
             cin.ignore();
 
             cout << "is the new choice correct? y/n" << endl;
             cin >> choiceAnswer;
+            
 
             if (choiceAnswer == 'Y' || choiceAnswer == 'y')
             {
                 // seting the rest choices to false 
                 for (int i = 0; i < 4; i++)
                 {
-                    questiontype[editQuestionChoiceIndex - 1].questionAnswer[4].isCorrect = false;
+                    questiontype[editQuestionChoiceIndex - 1].questionAnswer[i].isCorrect = false;
                 }
                 questiontype[editQuestionChoiceIndex - 1].questionAnswer[editChoiceIndex - 1].isCorrect = true;
             }
@@ -84,30 +141,41 @@ void modifyexam(question questiontype[], int ARRAYSIZE1)
             }
         }
         break;
-        
+
         // case he want to exit
-        case 'n':
-        case 'N':
+
+        case 4:
             loopcondition = false; // to exite the loop and go bake to main
             break;
-
-        }
-    }
-}
-
-
-
-void displayQuestion(int ARRAYSIZE1) {
-    for (int i = 0; i < ARRAYSIZE1; i++)
-    {
-        cout << i + 1 << "_ " << chemistryQuestionList[i].questionText << " ?" << endl;
-
-
-        for (int j = 0; j < 4; j++)
+        default:
         {
-            cout << j + 1 << "_ " << chemistryQuestionList[i].questionAnswer[j].choiceText << " ." << endl;
-
-
+            cout << "invalid choice, please enter again !";
+        }
         }
     }
 }
+
+void displayQuestion(question questiontype[]) {
+
+    for (int i = 0; i < 10; i++)
+    {
+        // Check if questionText is not empty or garbage
+        if (!chemistryQuestionList[i].questionText.empty()) {
+
+            cout << i + 1 << "_ " << chemistryQuestionList[i].questionText << " ?" << endl;
+
+            for (int j = 0; j < 4; j++)
+            {
+                // Check if choiceText is not empty or garbage
+                if (!chemistryQuestionList[i].questionAnswer[j].choiceText.empty())
+                {
+
+                    cout << j + 1 << "_ " << chemistryQuestionList[i].questionAnswer[j].choiceText << " ." << endl;
+                }
+
+
+            }
+        }
+    }
+}
+
